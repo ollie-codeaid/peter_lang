e1de014877a394745b5d776ff1966239ef0c0985
Ollie <reelfeet@hotmail.com>
2018-12-28 09:28:10 +0000

    Hook Artwork list into site

--- peter_lang/artworks/urls.py
+++ peter_lang/artworks/urls.py
@@ -8,7 +8,7 @@ from .views import (
 
 app_name = 'artworks'
 urlpatterns = [
-    path('', ArtworkList.as_view(), name='artwork-list'),
+    path('', ArtworkList.as_view(), name='list'),
     path('create', ArtworkCreate.as_view(), name='create'),
     path('<str:slug>/', ArtworkDetail.as_view(), name='detail'),
 ]
