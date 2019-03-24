8b1c88a59130fccd61ad82abd3ce85c40fa52464
Ollie <reelfeet@hotmail.com>
2019-03-24 16:38:01 +0000

    More homepage rejig

--- peter_lang/home/views.py
+++ peter_lang/home/views.py
@@ -10,6 +10,6 @@ class HomeView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['latest_articles'] = Article.objects.all()[:5]
+        context['latest_article'] = Article.objects.last()
 
         return context
