60ec0fcfccba29ca2706de112d3f07ac34b3ed4a
Ollie <reelfeet@hotmail.com>
2019-03-31 15:06:51 +0100

    Randomize queryset on homepage

--- peter_lang/home/views.py
+++ peter_lang/home/views.py
@@ -8,6 +8,9 @@ class HomeView(ListView):
     model = Artwork
     template_name = 'pages/home.html'
 
+    def get_queryset(self):
+        return super().get_queryset().order_by('?')[:10]
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_article'] = Article.objects.last()
