49ea6a979d1937908eea5cd1bd4af0515d3098c7
Ollie <reelfeet@hotmail.com>
2019-05-05 11:06:51 +0100

    Fix? CookieLaw

--- config/settings/base.py
+++ config/settings/base.py
@@ -243,7 +243,7 @@ ACCOUNT_AUTHENTICATION_METHOD = 'username'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_EMAIL_REQUIRED = True
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
 ACCOUNT_ADAPTER = 'peter_lang.users.adapters.AccountAdapter'
 # https://django-allauth.readthedocs.io/en/latest/configuration.html
